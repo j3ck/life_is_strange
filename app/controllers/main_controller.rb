9d266eb430cd0e00a445a794cb611c7db2313833
Riley Usagi <riley.usagi@gmail.com>
2015-10-16 09:52:36 +0300

    dashboard update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -20,6 +20,12 @@ class MainController < ApplicationController
       all_blogs_issues = blogs_milestone.open_issues + blogs_milestone.closed_issues
       @blogs_done = (blogs_milestone.closed_issues.to_f / (all_blogs_issues.to_f / 100)).round.inspect
 
+    # Вычисляем статистику по milestone Blogs
+      gym_milestone = github.issues.milestones.get 'riley-usagi', 'life_is_strange', '4'
+      all_gym_issues = gym_milestone.open_issues + gym_milestone.closed_issues
+      @gym_done = (gym_milestone.closed_issues.to_f / (all_gym_issues.to_f / 100)).round.inspect
+
+
   end
 
   private
