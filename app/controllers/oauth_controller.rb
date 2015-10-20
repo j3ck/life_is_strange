022c19e9b6494b1f251a3cf0f56ef69486f69ed6
Riley Usagi <riley.usagi@gmail.com>
2015-10-20 14:34:08 +0300

    fast fix

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -2,29 +2,24 @@ class OauthController < ApplicationController
   # Класс отвечает за создание сессий пользователей на сайте
 
   def authorize
-    client = OAuth2::Client.new(SLACK[:client_id],
-                                SLACK[:client_secret],
-                                site: SLACK[:authorize_uri])
+    client = OAuth2::Client.new(SLACK[:client_id], SLACK[:client_secret], site: SLACK[:authorize_uri])
     redirect_to client.auth_code.authorize_url(redirect_uri: SLACK[:redirect_uri])
   end
 
   def deauthorize
     session[:access_token] = nil
-    session[:user_id] = nil
+    session[:user_id]      = nil
     redirect_to root_url, notice: 'You have logged out of Slack.'
   end
 
   def request_access_token
     if params[:code]
-      client = OAuth2::Client.new(SLACK[:client_id],
-                                  SLACK[:client_secret],
-                                  site: SLACK[:authorize_uri],
-                                  token_url: SLACK[:token_uri])
-      token = client.auth_code.get_token(params[:code], redirect_uri: SLACK[:redirect_uri]).token
+      client   = OAuth2::Client.new(SLACK[:client_id], SLACK[:client_secret], site: SLACK[:authorize_uri], token_url: SLACK[:token_uri])
+      token    = client.auth_code.get_token(params[:code], redirect_uri: SLACK[:redirect_uri]).token
       response = HTTParty.get('https://slack.com/api/auth.test', query: { token: token })
 
       # Находим пользователя через метод в модели User (или создаём)
-      @user = User.find_or_create(token, response.parsed_response["user_id"])
+      @user             = User.find_or_create(token, response.parsed_response["user_id"])
       # Создаём сессию
       session[:user_id] = @user.id
 
